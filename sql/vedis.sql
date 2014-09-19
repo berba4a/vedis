42e9dcde39d7cb2048a765231a001af481e6b633
berba4a <berba4a@abv.bg>
2014-09-19 20:48:55 +0300

    19.09.2014-20.50
    Update Database records

--- sql/vedis.sql
+++ sql/vedis.sql
@@ -1,13 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.2.2
+-- version 4.0.4.1
 -- http://www.phpmyadmin.net
 --
 -- Хост: 127.0.0.1
 -- Време на генериране: 
--- Версия на сървъра: 5.5.27
--- Версия на PHP: 5.4.7
+-- Версия на сървъра: 5.5.32
+-- Версия на PHP: 5.4.19
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
@@ -19,6 +19,8 @@ SET time_zone = "+00:00";
 --
 -- БД: `vedis`
 --
+CREATE DATABASE IF NOT EXISTS `vedis` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+USE `vedis`;
 
 -- --------------------------------------------------------
 
@@ -41,7 +43,20 @@ CREATE TABLE IF NOT EXISTS `products` (
   KEY `typeID` (`typeID`),
   KEY `genderID` (`genderID`),
   KEY `usageID` (`usageID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=18 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=25 ;
+
+--
+-- Схема на данните от таблица `products`
+--
+
+INSERT INTO `products` (`productID`, `catalogueID`, `typeID`, `genderID`, `usageID`, `description`, `release_date`, `last_update`, `is_active`) VALUES
+(18, 1010, 1, 1, 1, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '2014-09-01', '2014-09-19 17:12:08', '1'),
+(19, 1020, 1, 2, 1, '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '2014-09-02', '2014-09-19 17:29:08', '1'),
+(20, 1030, 1, 3, 2, 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.', '2014-09-03', '2014-09-19 17:37:56', '1'),
+(21, 1040, 1, 1, 2, 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.', '2014-09-04', '2014-09-19 17:39:15', '1'),
+(22, 1050, 1, 2, 1, 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.', '2014-09-05', '2014-09-19 17:43:01', '1'),
+(23, 1060, 1, 3, 1, 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.', '2014-09-06', '2014-09-19 17:44:07', '0'),
+(24, 1070, 1, 2, 2, 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.', '2014-09-07', '2014-09-19 17:47:18', '1');
 
 -- --------------------------------------------------------
 
@@ -56,7 +71,7 @@ CREATE TABLE IF NOT EXISTS `product_gender` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
 
 --
--- Ссхема на данните от таблица `product_gender`
+-- Схема на данните от таблица `product_gender`
 --
 
 INSERT INTO `product_gender` (`genderID`, `name`) VALUES
@@ -76,7 +91,45 @@ CREATE TABLE IF NOT EXISTS `product_images` (
   `name` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`imageID`),
   KEY `productID` (`productID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=33 ;
+
+--
+-- Схема на данните от таблица `product_images`
+--
+
+INSERT INTO `product_images` (`imageID`, `productID`, `name`) VALUES
+(1, 18, 'model_1010_79554.jpg'),
+(2, 18, 'model_1010_27725.jpg'),
+(3, 18, 'model_1010_76911.jpg'),
+(4, 18, 'model_1010_67783.jpg'),
+(5, 18, 'model_1010_8789.jpg'),
+(6, 18, 'model_1010_95682.jpg'),
+(7, 18, 'model_1010_16629.jpg'),
+(8, 18, 'model_1010_98676.jpg'),
+(9, 19, 'model_1020_7632.JPG'),
+(10, 19, 'model_1020_74298.JPG'),
+(11, 19, 'model_1020_20541.JPG'),
+(12, 19, 'model_1020_46997.JPG'),
+(13, 19, 'model_1020_97855.JPG'),
+(14, 19, 'model_1020_64765.JPG'),
+(15, 19, 'model_1020_12619.JPG'),
+(16, 19, 'model_1020_89710.JPG'),
+(17, 20, 'model_1030_90577.JPG'),
+(18, 20, 'model_1030_35477.JPG'),
+(19, 20, 'model_1030_3070.JPG'),
+(20, 20, 'model_1030_75437.JPG'),
+(21, 20, 'model_1030_74304.JPG'),
+(22, 21, 'model_1040_23953.JPG'),
+(23, 21, 'model_1040_47015.JPG'),
+(24, 21, 'model_1040_65476.JPG'),
+(25, 21, 'model_1040_88562.JPG'),
+(26, 21, 'model_1040_97184.JPG'),
+(27, 22, 'model_1050_83695.JPG'),
+(28, 22, 'model_1050_71308.JPG'),
+(29, 24, 'model_1070_75803.JPG'),
+(30, 24, 'model_1070_26721.JPG'),
+(31, 24, 'model_1070_19034.JPG'),
+(32, 24, 'model_1070_79547.JPG');
 
 -- --------------------------------------------------------
 
@@ -91,7 +144,7 @@ CREATE TABLE IF NOT EXISTS `product_type` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
 
 --
--- Ссхема на данните от таблица `product_type`
+-- Схема на данните от таблица `product_type`
 --
 
 INSERT INTO `product_type` (`typeID`, `name`) VALUES
@@ -110,7 +163,7 @@ CREATE TABLE IF NOT EXISTS `product_usage` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
 
 --
--- Ссхема на данните от таблица `product_usage`
+-- Схема на данните от таблица `product_usage`
 --
 
 INSERT INTO `product_usage` (`usageID`, `name`) VALUES
